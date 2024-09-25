c2545e14db91b541d0b53fa5f6acfda545f3b75f
ApurvSapar21 <128551701+ApurvSapar21@users.noreply.github.com>
2024-09-25 23:36:50 +1000

    Update delete.sh

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,11 @@
-# Your Solution
+# delete.sh
+
+#!/bin/bash
+
+# Stop the script if any error occurs
+set -euo pipefail
+
+# Apply the Kubernetes deployment
+kubectl delete -f scripts/kubernetes/deployment.yaml
+
+echo "Microservices deleted successfully."
