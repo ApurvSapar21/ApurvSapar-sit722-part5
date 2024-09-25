622ed929f36411eba17fc076fbccad3603c9bc6d
ApurvSapar21 <128551701+ApurvSapar21@users.noreply.github.com>
2024-09-25 23:36:36 +1000

    Update build-image.sh

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,1 +1,12 @@
-# Your Solution
+#!/bin/bash
+
+#build-image.sh
+
+# Stop the script if any error occurs
+set -e
+
+docker build -t "${CONTAINER_REGISTRY}/book_catalog:${VERSION}" -f book_catalog/Dockerfile ./book_catalog
+
+docker build -t "${CONTAINER_REGISTRY}/inventory_management:${VERSION}" -f inventory_management/Dockerfile ./inventory_management
+
+echo "Docker images built successfully."
